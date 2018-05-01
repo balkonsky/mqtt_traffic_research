98f4919b27384efe92dd6de3909b94434ca22c56
Maksim Avramenko <mavramenko@yotateam.com>
2018-05-01 19:26:42 +0300

    first commit

--- mqtt_traffic_research_sample.R
+++ mqtt_traffic_research_sample.R
@@ -9,21 +9,14 @@ for(i in 1:30) {
     temp_list[[i]][1] <- NULL
   }
 }
-df <- as.data.frame(temp_list)
+df_sample <- as.data.frame(temp_list)
 
-for(i in 1:ncol(df)){
-  ifelse(i==1,colnames(df)[i] <- "interval", colnames(df)[i] <- (paste0('day_',i-1)))
+for(i in 1:ncol(df_sample)){
+  ifelse(i==1,colnames(df_sample)[i] <- "interval", colnames(df_sample)[i] <- (paste0('day_',i-1)))
 }
 
-ggplot(df,aes(x=df$day_10)) + 
+ggplot(df_sample,aes(x=df_sample$day_10)) + 
   geom_density(fill='white')
-
-
-ggplot(df,aes(x=df$day_10))+
-  geom_dotplot(binwidth = 3)
-
-ggplot(df, aes(x = df$day_10))+
-  geom_histogram(fill= 'white', col = 'black', binwidth = 1)
 
 
 for (i in 1:29){
