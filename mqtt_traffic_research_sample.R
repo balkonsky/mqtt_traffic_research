bc2be428a8488b62371efc3744d792263299e27e
Maksim Avramenko <mavramenko@yotateam.com>
2018-05-02 00:02:25 +0300

    add general set analyst

--- mqtt_traffic_research_sample.R
+++ mqtt_traffic_research_sample.R
@@ -6,13 +6,14 @@ list_sample <- list()
 for(i in 1:30) {
   dataset_path <- paste0(getwd(),'/data_set/',i,"_04_mqtt.xlsx")
   list_sample[[paste0("day_",i,sep="")]] <- read_excel(dataset_path,sheet = "groupvalue",col_names = FALSE)
+  row_name <- as.vector(unlist(list_sample[[i]][1]))
   list_sample[[i]][1] <- NULL
 }
 df_sample <- as.data.frame(list_sample)
-
 for(i in 1:ncol(df_sample)){
   colnames(df_sample)[i] <- (paste0('day_',i))
 }
+row.names(df_sample) <- row_name
 
 
 list_descriptive_statistics_sample <- list()
@@ -23,7 +24,6 @@ for(i in 1:length(list_sample)){
     list_descriptive_statistics_sample[[paste0("day_",i,sep="")]][3] <- (min(as.numeric(unlist(list_sample[[i]]))))
     list_descriptive_statistics_sample[[paste0("day_",i,sep="")]][4] <- (max(as.numeric(unlist(list_sample[[i]]))))
 }
-
 df_descriptive_statistics_sample <- as.data.frame(list_descriptive_statistics_sample)
 
 
@@ -35,7 +35,6 @@ x <- data.frame()
 for(i in 1:ncol(df_sample)){
 x <- rbind(x, data.frame(v=as.vector(unlist(list_sample[[i]])), type=paste0('day_',i)))
 }
-
 ggplot(data=x) + 
   geom_density(aes(x = v, fill=type, color=type))
 
